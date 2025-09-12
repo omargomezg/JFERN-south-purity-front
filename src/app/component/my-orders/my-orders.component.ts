091fb628e4b72593ccd6d1d9c373786433a75407
Omar Gomez Gomez <ogomez@kdu.cl>
2025-09-12 02:17:30 -0300

    feat(): improve payment result display with enhanced error handling and user guidance

--- src/app/component/my-orders/my-orders.component.ts
+++ src/app/component/my-orders/my-orders.component.ts
@@ -3,58 +3,64 @@ import {CommonClientService} from "../../client-module/service/common-client.ser
 import {AuthService} from "../../core/service";
 import {PaginationModel, SaleOrderInterface} from "../../core/model";
 import {PageEvent} from "@angular/material/paginator";
+import {Router} from "@angular/router";
 
 @Component({
-    selector: 'app-my-requests',
-    templateUrl: './my-orders.component.html',
-    styleUrls: ['./my-orders.component.scss']
+  selector: 'app-my-requests',
+  templateUrl: './my-orders.component.html',
+  styleUrls: ['./my-orders.component.scss']
 })
 export class MyOrdersComponent {
-    dataSource: SaleOrderInterface[];
-    pagination: PaginationModel = new PaginationModel()
+  dataSource: SaleOrderInterface[];
+  pagination: PaginationModel = new PaginationModel()
 
-    constructor(private commonClientService: CommonClientService,
-                private authService: AuthService) {
-        this.dataSource = [];
-        this.loadMyOrders();
-    }
+  constructor(private readonly commonClientService: CommonClientService,
+              private readonly router: Router,
+              private readonly authService: AuthService) {
+    this.dataSource = [];
+    this.loadMyOrders();
+  }
 
-    loadMyOrders(): void {
-        let id = this.authService.getProfile()?.id as string;
-        this.commonClientService.getSaleOrder(id, this.pagination).subscribe(orders => {
-            orders.content.forEach((order: SaleOrderInterface) => {
-                if (order.status !== 'APPROVED') {
-                    order.total = 0;
-                }
-                order.status = this.convertState(order.status);
-            });
-            this.dataSource = orders.content;
-            this.pagination.length = orders.totalElements;
-        });
-    }
-
-    convertState(state: string): string {
-        switch (state) {
-            case 'APPROVED':
-                return 'Aprobado';
-            case 'REJECTED':
-                return 'Rechazado';
-            case 'PENDING':
-                return 'Pendiente';
-            case 'TIMEOUT':
-                return 'Expirado';
-            case 'UNKNOWN':
-                return '--';
-            default:
-                return '---';
+  loadMyOrders(): void {
+    let id = this.authService.getProfile()?.id as string;
+    this.commonClientService.getSaleOrder(id, this.pagination).subscribe(orders => {
+      orders.content.forEach((order: SaleOrderInterface) => {
+        if (order.status !== 'APPROVED') {
+          order.total = 0;
         }
-    }
+        order.status = this.convertState(order.status);
+      });
+      this.dataSource = orders.content;
+      this.pagination.length = orders.totalElements;
+    });
+  }
 
-    handlePageEvent(pageEvent: PageEvent): void {
-        this.pagination.length = pageEvent.length;
-        this.pagination.pageSize = pageEvent.pageSize;
-        this.pagination.pageIndex = pageEvent.pageIndex;
-        this.loadMyOrders();
+  convertState(state: string): string {
+    switch (state) {
+      case 'APPROVED':
+        return 'Aprobado';
+      case 'REJECTED':
+        return 'Rechazado';
+      case 'PENDING':
+        return 'Pendiente';
+      case 'TIMEOUT':
+        return 'Expirado';
+      case 'UNKNOWN':
+        return '--';
+      default:
+        return '---';
     }
+  }
+
+  handlePageEvent(pageEvent: PageEvent): void {
+    this.pagination.length = pageEvent.length;
+    this.pagination.pageSize = pageEvent.pageSize;
+    this.pagination.pageIndex = pageEvent.pageIndex;
+    this.loadMyOrders();
+  }
+
+  buy() {
+    this.router.navigateByUrl('hacer-pedido');
+  }
 
 }
