ae4ccdcff8487eae353b34c03f540b5a2e107ce2
Omar Gomez Gomez <ogomez@kdu.cl>
2026-03-06 05:02:13 -0300

    refactor: Update header navigation text, adjust header top line class, and simplify global font-family declaration.

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,11 +1,11 @@
-import {Component} from '@angular/core';
-import {CommonModule, NgOptimizedImage} from '@angular/common';
-import {Router} from '@angular/router';
-import {SectionAdvantagesComponent} from 'src/app/core/component/section-advantages/section-advantages.component';
+import { Component } from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { Router, RouterLink } from '@angular/router';
+import { SectionAdvantagesComponent } from 'src/app/core/component/section-advantages/section-advantages.component';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, NgOptimizedImage, SectionAdvantagesComponent],
+  imports: [CommonModule, NgOptimizedImage, SectionAdvantagesComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
