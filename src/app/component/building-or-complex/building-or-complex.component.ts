e89fd8bd277e1d68a70c927b723c32c0e510dd29
Omar Gómez <omar.fdo.gomez@gmail.com>
2026-05-21 17:06:42 -0400

    refactor: improve clipboard copy functionality and integrate snackbar notifications

--- src/app/component/building-or-complex/building-or-complex.component.ts
+++ src/app/component/building-or-complex/building-or-complex.component.ts
@@ -1,6 +1,6 @@
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { Component, inject, OnInit, signal } from '@angular/core';
-
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-building-or-complex',
@@ -12,8 +12,9 @@ export class BuildingOrComplexComponent implements OnInit {
   private breakpointObserver = inject(BreakpointObserver);
   isMobile = signal<boolean>(false);
   copiedMessage: string = '';
-  private emailSubject = "Hola, podemos contratar agua purificada en mi comunidad";
-  private emailBody = "Puedes revisar el sitio www.purezadelsur.cl y contratar el servicio sin costo!"
+  private emailSubject = "Hola, podemos contratar agua purificada en mi comunidad";;
+  private emailBody = "Puedes revisar el sitio www.purezadelsur.cl y contratar el servicio sin costo!";
+  private snackBar = inject(MatSnackBar);
 
    ngOnInit(): void {
     this.breakpointObserver.observe(['(max-width: 767px)']).subscribe(result => {
@@ -22,14 +23,12 @@ export class BuildingOrComplexComponent implements OnInit {
   }
 
   copyToClipboardAndSendToEmail() {
-    const emailAddress = 'contacto@example.com'; // Reemplazar con el email de contacto real
+    const emailAddress = 'purezadelsur@gmail.com';
     navigator.clipboard.writeText(emailAddress).then(() => {
-      this.copiedMessage = 'Copiado al portapapeles!';
+      this.snackBar.open('Correo copiado al portapapeles!', 'Cerrar', { duration: 3000 });
       setTimeout(() => {
-        this.copiedMessage = '';
-        // Abrir cliente de correo predeterminado con subject
         window.open(`mailto:?subject=${encodeURIComponent(this.emailSubject)}&body=${encodeURIComponent(this.emailBody)}`, '_blank');
-      }, 3000); // Mostrar mensaje por 3 segundos
+      }, 3000);
     }).catch(err => {
       console.error('Error al copiar: ', err);
       this.copiedMessage = 'Error al copiar';
@@ -39,12 +38,10 @@ export class BuildingOrComplexComponent implements OnInit {
   copyToClipboardAndSendToWhatsApp() {
     const link = '+56984428760';
     navigator.clipboard.writeText(link).then(() => {
-      this.copiedMessage = 'Copiado al portapapeles!';
+      this.snackBar.open('Enlace copiado al portapapeles!', 'Cerrar', { duration: 3000 });
       setTimeout(() => {
-        this.copiedMessage = '';
-        // open new whatsapp window after message disappears
         window.open(`https://wa.me`, '_blank');
-      }, 3000); // Hide message after 3 seconds
+      }, 3000);
     }).catch(err => {
       console.error('Error al copiar: ', err);
       this.copiedMessage = 'Error al copiar';
