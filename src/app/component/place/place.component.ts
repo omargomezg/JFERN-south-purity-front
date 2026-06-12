e5e12e67bd35b1038920fe0625a88a6bdace4534
Omar Gómez <omar.fdo.gomez@gmail.com>
2026-06-11 22:03:34 -0400

    feat: enhance place component with loading skeleton and role-based UI adjustments

--- src/app/component/place/place.component.ts
+++ src/app/component/place/place.component.ts
@@ -1,31 +1,51 @@
-import {Component} from '@angular/core';
+import {Component, inject, OnInit, signal} from '@angular/core';
 import {MatDialog} from "@angular/material/dialog";
 import {FormPlaceComponent} from "../form-place/form-place.component";
-import {CommonAdminService, PlaceService} from "../../core/service";
+import {AuthService, CommonAdminService, PlaceService} from "../../core/service";
 import {PlaceInterface} from "../../core/model";
 import {Router} from '@angular/router';
 import {MatSlideToggleChange} from "@angular/material/slide-toggle";
 import {PlaceStatusEnum} from "../../core/constant/app.constants";
 import {ToastrService} from "ngx-toastr";
+import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
+import {RoleEnum} from "../../core/constant/role.enum";
 
 @Component({
   selector: 'app-place',
   templateUrl: './place.component.html',
   styleUrls: ['./place.component.scss']
 })
-export class PlaceComponent {
+export class PlaceComponent implements OnInit {
   displayedColumns: string[] = ['city', 'name', 'status', 'options'];
   dataSource: PlaceInterface[] = [];
+  private breakpointObserver = inject(BreakpointObserver);
+  isMobile = signal<boolean>(false);
+  role: string = RoleEnum.STOCKER;
+  loading: boolean = false;
 
   constructor(private dialog: MatDialog, private commonAdminService: CommonAdminService,
               private router: Router,
               private placeService: PlaceService,
+              private authService: AuthService,
               private toastr: ToastrService) {
+    this.loading = true;
+  }
+
+  ngOnInit(): void {
+    this.role = this.authService.getProfile()?.role || RoleEnum.ROLE_ANONYMOUS;
     this.loadPlaces();
+    this.breakpointObserver
+      .observe([Breakpoints.Handset])
+      .subscribe(result => {
+        this.isMobile.set(result.matches);
+      });
   }
 
   loadPlaces(): void {
-    this.commonAdminService.getPlaces().subscribe(places => this.dataSource = places);
+    this.commonAdminService.getPlaces().subscribe(places => {
+      this.dataSource = this.role === RoleEnum.STOCKER ? places.filter(x => x.status === "ENABLED") : places;
+      this.loading = false;
+    });
   }
 
   add() {
